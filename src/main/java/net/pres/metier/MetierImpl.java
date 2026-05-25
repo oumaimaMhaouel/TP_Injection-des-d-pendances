a424889d072f3e5a0cbd0be99ae23f0e7300491c
oumaima.mhaouel@gmail.com <your_emailexample.com>
2026-05-25 16:53:00 +0100

    màj la couche metier

--- src/main/java/net/pres/metier/MetierImpl.java
+++ src/main/java/net/pres/metier/MetierImpl.java
@@ -1,20 +1,38 @@
 package net.pres.metier;
 
 import net.pres.dao.IDao;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
-/**
- * Couche métier sans Spring — injection manuelle (Pres1, Pres2, Pres3 / JAXB).
- */
-public class MetierImpl extends MetierBase {
+@Component("metier")
+public class MetierImpl implements IMetier {
 
-    public MetierImpl() {
-    }
+    private IDao iDao;
 
-    public MetierImpl(IDao iDao) {
+    /**
+     * Pour injecter dans l'attribut dao un objet d'une classe qui implémente l'interface IDao
+     * au moment de l'instanciation
+     */
+    public MetierImpl(@Qualifier("d") IDao iDao) {
         this.iDao = iDao;
     }
 
+    @Override
+    public double calcul() {
+        double temperature = iDao.getData();
+        double res = temperature * 12 * Math.PI / 2 * Math.cos(temperature);
+        return res;
+    }
+
+    /**
+     * Pour injecter dans l'attribut dao un objet d'une classe qui implémente l'interface IDao
+     *
+     * @param iDao
+     */
     public void setDao(IDao iDao) {
         this.iDao = iDao;
     }
+
+
 }
+
